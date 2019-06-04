936bc31c0058059ee22a39fae8f82f5f5e7e5680
Brittany Alleman <brittany.alleman1@gmail.com>
2019-06-04 15:39:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,9 @@ require_relative ../conference_badges
 
 def badge_maker(name)
   return "Hello, my name is #{name}."
-
+end
 
 def batch_badge_creator(array)
   return array.each do "hello, my name is"
 end 
- end
+ 
